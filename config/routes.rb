ba66da171aa6674c3de6bd4e3701d1d95793056e
catkhuu <sunshine.dumbo@gmail.com>
2017-03-22 15:32:59 -0400

    Namespace and scope routes for API versioning. Add routes for Sessions and Users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'api' do
+    namespace :v1 do
+      resources :sessions, only: [:create, :destroy]
+      resources :users, only: [:create, :show, :update]
+    end
+  end 
 end
