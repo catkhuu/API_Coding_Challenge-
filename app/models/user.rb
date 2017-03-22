2b49bf42653d79d833e4224830d4ea607add0915
catkhuu <sunshine.dumbo@gmail.com>
2017-03-22 15:35:46 -0400

    Include has_secure_password for user authentication. Create methods to set and generate auth_token on User after each login.

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  def self.generate_auth_token(user)
+    loop do
+      token = SecureRandom.hex
+      break token unless user.class.exists?(auth_token: token)
+    end
+  end
+
+  def self.set_auth_token(user)
+    return if user.auth_token.present?
+    user.auth_token = self.generate_auth_token(user)
+  end
+end
